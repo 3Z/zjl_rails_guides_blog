31e9d8562f005b5b2803dcbc1efdf3234582dfeb
zhujinlong <zhu_jinlong@lbxtech.com>
2014-10-11 22:48:18 +0800

    MOD delete action done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,9 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    #must new a article here, otherwise new.html.erb @article will be nil when call new_article_path
+    #(create action fails and redirect to new action will be safe, since @article passed from create will always not be nil)
+    #nil can not call @article.errors.any? (_form.html.erb)
     @article = Article.new
   end
 
@@ -14,9 +17,11 @@ class ArticlesController < ApplicationController
   	@article = Article.new(article_params)
 
     if @article.save
-      #TODO if there existing create.html.erb in views, not return immediately
-      #will cause render create.html.erb?
-  	  redirect_to article_path(@article) and return
+      #no need add "and return, redirect_to will stop render create.html.erb if exists"
+  	  redirect_to article_path(@article)
+      #although add "and return" here, still rendering create.html.erb if exists
+      #TODO that means "and return" no use?
+      #puts "test" and return
     else
       render 'new'
     end
@@ -43,6 +48,13 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
 
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
