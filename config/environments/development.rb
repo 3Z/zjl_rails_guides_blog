83f8b910b0be79d24d03fddc2c9f637d05a7e1f5
zhu_jinlong <zhu_jinlong@163.com>
2014-12-28 21:40:22 +0800

    MOD make devise signin/signup running

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,17 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.126.com",
+      :port => 25,
+      :domain => "126.com",
+      :authentication => :login,
+      :user_name => "zhu_jinlong007@126.com", #你的邮箱
+      :password => "jiangnanmaiL" #你的密码
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
