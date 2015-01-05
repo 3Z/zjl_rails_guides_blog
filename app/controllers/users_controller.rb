43529e0666d2f0598b55b6e11e2156d177d77c9e
zhu_jinlong <zhu_jinlong@163.com>
2015-01-05 23:22:10 +0800

    MOD make user's articles runing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,54 +1,25 @@
 class UsersController < ApplicationController
 
-  has_scope :by_gender, :type => :default
-  has_scope :haha_age, :type => :default
+  # has_scope :by_gender, :type => :default
+  # has_scope :haha_age, :type => :default
 
   def index
     # @users = User.all
     # @users = User.where(gender: params[:gender]).where("age < ?", params[:age])
-    @users = apply_scopes(User).all
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to users_path
-    else
-      render 'new'
-    end
+    # @users = apply_scopes(User).all
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-  end
+    @articles = @user.articles
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-
-    if @user.update(user_params)
-      redirect_to user_path(@user)
-    else
-      render 'edit'
+    puts "--------"
+    puts @articles
+    @articles.each do |article|
+      puts article.title
     end
+
   end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to users_path
-  end
-
-  private
-    def user_params
-      params.require(:user).permit(:name, :gender, :age, :email)
-    end
 end
