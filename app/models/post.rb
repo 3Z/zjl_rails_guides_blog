6d9fd57f89d1d1819158aafaad21f90514134c60
zhu_jinlong <zhu_jinlong@163.com>
2015-02-05 23:28:53 +0800

    MOD add validates to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
-  # validates :title, presence: true, length: { minimum: 5}
+
+  validates :title, presence: true, length: { minimum: 10}
+  validates :text, presence: true, length: { minimum: 20}
+
 end
